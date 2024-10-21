17a9fdb29eaaaf9472236b809acb728beb4db0d1
MiDoM05 <michaldomurat2567@gmail.com>
2024-10-21 09:25:48 +0200

    probny

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        float x = 5;
-        float y = 6;
-        System.out.println(x / y);
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        final int MAX_LICZBA = 21/2;
+        int los = (int)(Math.random() * MAX_LICZBA + 1);
+        int odp = 0;
+        int liczbProb=0;
+        do {
+            System.out.format("Podaj liczbe od 1 do %d (0 - konczy gre):", MAX_LICZBA);
+            odp = sc.nextInt();
+            if( 0 == odp ) break;
+            ++liczbProb;
+        } while ( odp != los );
+        if( odp == los ) System.out.println( "Szukana liczba 6to " + los + ". Bylo " + liczbProb + " prob" );
+        else System.out.println( "Koniec gry" );
     }
 }
